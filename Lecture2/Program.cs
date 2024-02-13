309461803d45f842cc3fa3be8e7b7083166dad8b
elina <elina.kalnina@gmail.com>
2024-02-13 10:40:05 +0200

    Add project files.

---
+++ Lecture2/Program.cs
@@ -0,0 +1,5 @@
+﻿using Lecture2;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+TestMethodOverload.transformtotext(true);
---
+++ Lecture2/TestMethodOverload.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture2
+{
+    public static class TestMethodOverload
+    {
+        public static string transformtotext(int i)
+        {
+            Debug.Print("int version");
+            return i.ToString();
+        }
+
+       public static string transformtotext(bool b)
+        {
+            Debug.Print("bool version");
+            return b.ToString();
+        }
+
+        public static string transformtotext(bool b, int i)
+        {
+            Debug.Print("bool version");
+            return b.ToString();
+        }
+    }
+}
